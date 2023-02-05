818cc7dbe5d3543560aa4ef7452367c92fd7fc91
Juha Leikas <juha.leikas@gmail.com>
2023-02-05 17:08:33 +0200

    jinja test

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
