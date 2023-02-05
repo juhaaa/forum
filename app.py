adf12725ac54f889253ac5a08db0930bf7e9f487
Juha Leikas <juha.leikas@gmail.com>
2023-02-05 18:04:31 +0200

    html- structure

--- app.py
+++ app.py
@@ -12,6 +12,18 @@ db = SQLAlchemy(app)
 def index():
     return render_template("index.html")
 
+@app.route("/discussion")
+def discussion():
+    return render_template("discussion.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("/register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
