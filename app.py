a2913b338da6069c8b2012bd476ca3690a8bb7b7
Juha Leikas <juha.leikas@gmail.com>
2023-02-05 20:37:55 +0200

    Trying to figure out url arguments

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
@@ -14,15 +15,30 @@ def index():
 
 @app.route("/discussion")
 def discussion():
-    return render_template("discussion.html")
+    sql = text("SELECT id, name FROM discussion_zones ORDER BY id")
+    result = db.session.execute(sql)
+    zones =  result.fetchall()
+    return render_template("discussion.html", zones=zones)
 
-@app.route("/login")
+@app.route("/<name>")
+def zone(name):
+    sql = text("SELECT name, id FROM discussion_zones WHERE name=:name")
+    result = db.session.execute(sql,  {"name":name})
+    zone =  result.fetchone()
+    return render_template("zone.html", name=name, zone=zone)
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
 @app.route("/register")
 def register():
-    return render_template("/register.html")
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
---
+++ schema_test_add.sql
@@ -0,0 +1,9 @@
+INSERT INTO discussion_zones (name) VALUES ('General Chat');
+INSERT INTO discussion_zones (name) VALUES ('Python');
+INSERT INTO discussion_zones (name) VALUES ('Java');
+INSERT INTO discussion_zones (name) VALUES ('C++');
+INSERT INTO discussion_zones (name) VALUES ('Haskell');
+INSERT INTO discussion_zones (name) VALUES ('Ruby');
+INSERT INTO discussion_zones (name) VALUES ('Rockstar language');
+INSERT INTO users (username, password) VALUES ('juhaaa', 'jaska123');
+INSERT INTO users (username, password, is_admin) VALUES ('admin', 1234, TRUE);
