e0e739909e99e0b0747700fdd28ac9ee11999545
Juha Leikas <juha.leikas@gmail.com>
2023-02-11 17:17:47 +0200

    added capability to view posts and replies

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ db = SQLAlchemy(app)
 def index():
     return render_template("index.html")
 
-@app.route("//discussion")
+@app.route("/discussion")
 def discussion():
     sql = text("SELECT id, name FROM discussion_zones ORDER BY id")
     result = db.session.execute(sql)
@@ -26,10 +26,20 @@ def zone(name):
     sql = text("SELECT name, id FROM discussion_zones WHERE name=:name")
     result = db.session.execute(sql,  {"name":name})
     zone =  result.fetchone()
-    sql2 = text("SELECT topics.id, topics.user_id, topics.title, topics.content, users.username, created_at FROM topics LEFT JOIN users ON users.id=topics.user_id WHERE topics.discussion_zone_id=:zoneid ORDER BY topics.created_at DESC")
+    sql2 = text("SELECT topics.id, topics.user_id, topics.title, topics.content, users.username, topics.created_at FROM topics LEFT JOIN users ON users.id=topics.user_id WHERE topics.discussion_zone_id=:zoneid ORDER BY topics.created_at DESC")
     result2 = db.session.execute(sql2, {"zoneid":zone.id})
     topics = result2.fetchall()
     return render_template("/zone.html", name=name, zone=zone, topics=topics)
+
+@app.route("/discussion/<name>/<topic_id>")
+def topic(name, topic_id):
+    sql = text("SELECT topics.id, topics.title, topics.content, topics.created_at, users.username FROM topics LEFT JOIN users ON users.id=topics.user_id WHERE topics.id=:topic_id")
+    result = db.session.execute(sql, {"topic_id":topic_id})
+    topic = result.fetchone()
+    sql2 = text("SELECT users.username, replies.content, replies.created_at FROM replies LEFT JOIN users ON  users.id=replies.user_id WHERE topic_id=:topic_id")
+    result2 = db.session.execute(sql2, {"topic_id":topic_id})
+    replies = result2.fetchall()
+    return render_template("/topic.html", name=name, topic_id=topic_id, topic=topic, replies=replies)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
